9f30d83a09dec4da069e69e45e51fd888c4ef471
Raj Vishwakarma <rajrv6929@gmail.com>
2023-09-24 00:28:27 +0530

    Added Huffman encoding theory codes

---
+++ CHAPTER_2/2.3/2.3.4/huffman-decoding.rkt
@@ -0,0 +1,21 @@
+#lang scheme
+(require "huffman-representation.rkt")
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+      '()
+      (let ((next-branch
+              (choose-branch (car bits) current-branch)))
+        (if (leaf? next-branch)
+          (cons (symbol-leaf next-branch)
+                (decode-1 (cdr bits) tree))
+          (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(provide (all-defined-out))
