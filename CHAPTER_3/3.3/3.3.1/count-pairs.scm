1420de37827871354e17eba81ab5d5d56c1aa170
Raj Vishwakarma <rajrv6929@gmail.com>
2024-02-03 18:58:36 +0530

    Added count-pairs.scm

--- CHAPTER_3/3.3/3.3.1/count-pairs.scm
+++ CHAPTER_3/3.3/3.3.1/count-pairs.scm
@@ -7,6 +7,17 @@
          (count-pairs (cdr x))
          1)))
 
+(define (improved-count-pairs x)
+  (let ((counted-pairs '()))
+    (define (helper x) (cond ((not (pair? x)) 0)
+                             ((memq x counted-pairs) 0)
+                             (else (set! counted-pairs
+                                         (cons x counted-pairs))
+                                   (+ (helper (car x))
+                                      (helper (cdr x))
+                                      1))))
+    (helper x)))
+
 (define x3 '(a b c))
 (define m '(a))
 (define n (cons m m))
@@ -14,3 +25,18 @@
 (define x4 (cons 'a n))
 (define l (cons 'a m))
 (define x5 (cons l l))
+
+#|
+x3
+(count-pairs x3)
+(improved-count-pairs x3)
+x4
+(count-pairs x4)
+(improved-count-pairs x4)
+x5
+(count-pairs x5)
+(improved-count-pairs x5)
+x7
+(count-pairs x7)
+(improved-count-pairs x7)
+|#
