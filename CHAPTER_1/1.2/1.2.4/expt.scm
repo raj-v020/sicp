277fcfdd01dc0b0b44f057328cabff5f86a17597
Raj Vishwakarma <rajrv6929@gmail.com>
2024-02-19 02:21:06 +0530

    Added invariant exponentiation in expt.scm

--- CHAPTER_1/1.2/1.2.4/expt.scm
+++ CHAPTER_1/1.2/1.2.4/expt.scm
@@ -10,6 +10,21 @@
     (expti b n))
 
 (define (even? n)
-(= (remainder n 2) 0))
+  (= (remainder n 2) 0))
 
-(expt 2 10)
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (iexpt b n)
+  (define (invar-expt p q a)
+    (cond ((= q 0) a)
+          ((even? q) (invar-expt (square p) (/ q 2) a))
+          (else (invar-expt p (- q 1) (* p a)))))
+  (invar-expt b n 1))
+
+  
+
+(fast-expt 2 10)
+(iexpt 2 10)
