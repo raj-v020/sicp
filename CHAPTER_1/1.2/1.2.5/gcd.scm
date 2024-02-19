3926bac8758544f98c6690e1746d12e23944db32
Raj Vishwakarma <rajrv6929@gmail.com>
2024-02-19 10:56:30 +0530

    Made changes to gcd.scm

--- CHAPTER_1/1.2/1.2.5/gcd.scm
+++ CHAPTER_1/1.2/1.2.5/gcd.scm
@@ -1,5 +1,8 @@
 #lang sicp
-(define (gcd a b i)
-  (display i) (newline)
-  (if (= b 0) a (gcd b (remainder a b) (+ i 1))))
-(gcd 206 40 0)
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(gcd 206 40)
